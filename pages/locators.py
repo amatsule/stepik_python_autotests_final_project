d8d2aa3baeec7d30fd217a0ea4611e08dd8048ee
Matculevich <artur.matculevich@t-systems.com>
2022-08-18 11:53:56 +0300

    added h/w for step 4

--- pages/locators.py
+++ pages/locators.py
@@ -14,3 +14,11 @@ class LoginPageLocators():
     PASSWORD_CONFIRM_REGISTRATION_FIELD = (By.CSS_SELECTOR, "#id_registration-password2")
     REGISTER_BUTTON = (By.NAME, "registration_submit")
 
+
+class ProductPageLocators():
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    HAS_BEEN_ADDED_TO_BASKET_MESSAGE = (By.XPATH, "//div[@id='messages']//strong")
+    BASKET_TOTAL_MESSAGE = (By.XPATH, "//p[contains(text(),'Your basket total')]//strong ")
+    PRODUCT_NAME_STRING = (By.CSS_SELECTOR, ".product_main h1")
+    PRICE_STRING = (By.CSS_SELECTOR, ".product_main .price_color")
+
---
+++ pages/product_page.py
@@ -0,0 +1,24 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def click_add_to_basket_button(self):
+        self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON).click()
+
+    def get_product_name(self):
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_STRING).text
+
+    def get_product_price(self):
+        return self.browser.find_element(*ProductPageLocators.PRICE_STRING).text
+
+    def check_added_to_basket(self, product_name):
+        assert self.is_element_present(*ProductPageLocators.HAS_BEEN_ADDED_TO_BASKET_MESSAGE), \
+            "ADDED TO BASKET message does not present"
+        assert self.browser.find_element(*ProductPageLocators.HAS_BEEN_ADDED_TO_BASKET_MESSAGE).text == \
+               product_name, "Book name doesn't match"
+
+    def check_basket_total(self, product_price):
+        assert self.is_element_present(*ProductPageLocators.BASKET_TOTAL_MESSAGE), \
+            "BASKET TOTAL message doesn't present"
+        assert self.browser.find_element(*ProductPageLocators.BASKET_TOTAL_MESSAGE).text == product_price, \
+            "Basket total doesn't match"
